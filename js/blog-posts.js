785efb9a118276e6d732e49f2ba4784983f644c7
north577 <bugslugboy@gmail.com>
2026-07-28 18:36:23 -0400

    Update blog-posts.js

--- js/blog-posts.js
+++ js/blog-posts.js
@@ -4,8 +4,8 @@ const posts = [
     date: '2026-07-28',
     content: '<h3>this is a very good test.</h3><br><img src="../assets/crispy-air-fryer-onion-rings-recipe-0775.jpg"><br> yay<br> yaygkjdflshgiufdkghjefsihulghsuiedguhfil im retarrrdedddddddddddddd yayaayayayayay 2200c jidfogkliuhgofejhyuetrwykuthrfdytruawesJ FgyukaJSDuihlgkfrdeshiugltrsd gukyhitu467b8y8ol hrtiugxf'
   },
-{title: 'Doing this from my phone!!'
-date: '2026-07-28'
+{title: 'Doing this from my phone!!',
+date: '2026-07-28',
 content: '<h4>Super exciting that i can now sperg about whatever i want right from the comfort of my magic rectangle 🥹</h4>'
 }
 ];
