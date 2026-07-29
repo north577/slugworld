bb44088e9bd2886679f3111508e5f246d6cadbaf
north577 <bugslugboy@gmail.com>
2026-07-28 23:41:49 -0400

    editts

--- js/blog-posts.js
+++ js/blog-posts.js
@@ -2,8 +2,13 @@ const posts = [
   {
     title: 'EVERYONE LOVES ME!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!',
     date: '2026-07-28',
-    content: '### this is a very good test.\n\n![crispy onion rings](assets/crispy-air-fryer-onion-rings-recipe-0775.jpg)\n\nThis is a **great** test post with *emphasis* and a [link](https://example.com).'
-  }
+    content: '### this is a very good test.\n\n![crispy onion rings](assets/crispy-air-fryer-onion-rings-recipe-0775.jpg)\n\nThis is a **fantastic** test post with *emphasis* and a [link](https://example.com).'
+  },
+  {
+    title: 'the night of',
+    date: '2026-07',
+    content: 'testing\ the syntax *this* freaking silly **AWESOME** >>>shit  
+      #yes'
 ];
 
 function escapeHtml(text) {
